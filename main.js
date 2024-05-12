4cb68ed64db2e6d5acdef1cd5a76b4adb5bb809e
TurkyYassin <161338787+TurkyYassin@users.noreply.github.com>
2024-05-12 17:02:15 +0300

    d

---
+++ main.js
@@ -0,0 +1,39 @@
+function test1(){
+    document.getElementsByClassName('offers-items').styl.display = "block";
+    document.getElementsByClassName('one').styl.display="block";
+    document.getElementsByClassName('two').styl.display="block";
+    document.getElementsByClassName('three').styl.display="block";
+    document.getElementsByClassName('four').styl.display="block";
+}
+
+function test2(){
+    document.getElementsByClassName('one').styl.display = "block";
+    document.getElementsByClassName('offers-items').styl.display = "none"
+    document.getElementsByClassName('two').styl.display="none";
+    document.getElementsByClassName('three').styl.display="none";
+    document.getElementsByClassName('four').styl.display="none";
+}
+
+function test3(){
+    document.getElementsByClassName("offers-items").styl.display = "none";
+    document.getElementsByClassName("one").styl.display = "none";
+    document.getElementsByClassName("two").styl.display = "block";
+    document.getElementsByClassName("three").styl.display="none";
+    document.getElementsByClassName("four").styl.display="none";
+}
+
+function test4(){
+    document.getElementsByClassName("offers-items").styl.display = "none";
+    document.getElementsByClassName("one").styl.display = "none";
+    document.getElementsByClassName("two").styl.display = "none";
+    document.getElementsByClassName("three").styl.display="block";
+    document.getElementsByClassName("four").styl.display="none";
+}
+
+function test5(){
+    document.getElementsByClassName("offers-items").styl.display = "none";
+    document.getElementsByClassName("one").styl.display = "none";
+    document.getElementsByClassName("two").styl.display = "none";
+    document.getElementsByClassName("three").styl.display="none";
+    document.getElementsByClassName("four").styl.display="block";
+}
