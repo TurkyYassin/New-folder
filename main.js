19612f41263553dccd63f32209cbaab53ba101d3
mahmoud <bewpspecialist@gmail.com>
2024-06-04 12:20:06 +0300

    ل

--- main.js
+++ main.js
@@ -1,39 +1,54 @@
 function test1(){
-    document.getElementsByClassName('offers-items').styl.display = "block";
-    document.getElementsByClassName('one').styl.display="block";
-    document.getElementsByClassName('two').styl.display="block";
-    document.getElementsByClassName('three').styl.display="block";
-    document.getElementsByClassName('four').styl.display="block";
+    const offerlistelm=document.getElementById('offers-list');
+    offerlistelm.classList.remove('show-all','show-one','show-two','show-three','show-four');
+    offerlistelm.classList.add('show-all');
+    document.getElementById('offers-list').style.display = "block";
+    document.getElementById('one').style.display="block";
+    document.getElementById('two').style.display="block";
+    document.getElementById('three').style.display="block";
+    document.getElementById('four').style.display="block";
 }
 
 function test2(){
-    document.getElementsByClassName('one').styl.display = "block";
-    document.getElementsByClassName('offers-items').styl.display = "none"
-    document.getElementsByClassName('two').styl.display="none";
-    document.getElementsByClassName('three').styl.display="none";
-    document.getElementsByClassName('four').styl.display="none";
+    const offerlistelm=document.getElementById('offers-list');
+    offerlistelm.classList.remove('show-all','show-one','show-two','show-three','show-four');
+    offerlistelm.classList.add('show-one');
+    document.getElementById('one').style.display = "block";
+    document.getElementById('offers-list').style.display = "none";
+    document.getElementById('two').style.display="none";
+    document.getElementById('three').style.display="none";
+    document.getElementById('four').style.display="none";
 }
 
 function test3(){
-    document.getElementsByClassName("offers-items").styl.display = "none";
-    document.getElementsByClassName("one").styl.display = "none";
-    document.getElementsByClassName("two").styl.display = "block";
-    document.getElementsByClassName("three").styl.display="none";
-    document.getElementsByClassName("four").styl.display="none";
+    const offerlistelm=document.getElementById('offers-list');
+    offerlistelm.classList.remove('show-all','show-one','show-two','show-three','show-four');
+    offerlistelm.classList.add('show-two');
+    document.getElementById("offers-list").style.display = "none";
+    document.getElementById("one").style.display = "none";
+    document.getElementById("two").style.display = "block";
+    document.getElementById("three").style.display="none";
+    document.getElementById("four").style.display="none";
 }
 
 function test4(){
-    document.getElementsByClassName("offers-items").styl.display = "none";
-    document.getElementsByClassName("one").styl.display = "none";
-    document.getElementsByClassName("two").styl.display = "none";
-    document.getElementsByClassName("three").styl.display="block";
-    document.getElementsByClassName("four").styl.display="none";
+    const offerlistelm=document.getElementById('offers-list');
+    offerlistelm.classList.remove('show-all','show-one','show-two','show-three','show-four');
+    offerlistelm.classList.add('show-three');
+    document.getElementById("offers-list").style.display = "none";
+    document.getElementById("one").style.display = "none";
+    document.getElementById("two").style.display = "none";
+    document.getElementById("three").style.display="block";
+    document.getElementById("four").style.display="none";
 }
 
 function test5(){
-    document.getElementsByClassName("offers-items").styl.display = "none";
-    document.getElementsByClassName("one").styl.display = "none";
-    document.getElementsByClassName("two").styl.display = "none";
-    document.getElementsByClassName("three").styl.display="none";
-    document.getElementsByClassName("four").styl.display="block";
+    const offerlistelm=document.getElementById('offers-list');
+    offerlistelm.classList.remove('show-all','show-one','show-two','show-three','show-four');
+    offerlistelm.classList.add('show-four');
+    document.getElementById("offers-list").style.display = "none";
+    document.getElementById("one").style.display = "none";
+    document.getElementById("two").style.display = "none";
+    document.getElementById("three").style.display="none";
+    document.getElementById("four").style.display="block";
 }
